7dccc8465ec6b2f1d9f418b62f7089e43151457e
Abo Hassan <85116158+Memati7iq@users.noreply.github.com>
2025-08-05 15:30:38 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -355,12 +355,12 @@ function loadajbsettings(){
     document.getElementById('jailbreak-page').style.display = 'block';
     document.getElementById('PS4FW').style.display = 'flex';
     document.getElementById('payloads-page').style.display = 'none';
-    document.getElementById('payloadsbtn').textContent = 'Payloads';
+    document.getElementById('payloadsbtn').textContent = 'البايلودز';
   } else {
     document.getElementById('jailbreak-page').style.display = 'none';
     document.getElementById('PS4FW').style.display = 'none';
     document.getElementById('payloads-page').style.display = 'block';
-    document.getElementById('payloadsbtn').textContent = 'Jailbreak';
+    document.getElementById('payloadsbtn').textContent = 'جلبريك';
     localStorage.setItem('visibleDiv', 'payloads-page');
   }
 }
